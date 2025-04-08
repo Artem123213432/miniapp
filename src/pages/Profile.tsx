00dc25990c9756950dd2727aa28726104566526a
Artem123213432 <artemulitin@MacBook-Pro-Artem.local>
2025-04-08 14:29:41 +0300

    Switch to HashRouter and update build files

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserCircleIcon, PencilIcon } from '@heroicons/react/24/outline';
+import { PencilIcon } from '@heroicons/react/24/outline';
 
 const Profile: React.FC = () => {
   return (
